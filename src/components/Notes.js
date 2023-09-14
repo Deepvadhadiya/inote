ca01e4029ed23481371b358425b89f91be6d5a04
Deep <deepvadhadiya@gmail.com>
2023-09-14 18:22:38 +0530

    updating

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -8,9 +8,9 @@ const Notes = () => {
 
     const context = useContext(noteContext);
 
-    const [note, setNote] = useState({etitle: "", edescription: "", etag: ""});
+    const [note, setNote] = useState({id: "", etitle: "", edescription: "", etag: ""});
 
-    const { notes, getNotes } = context
+    const { notes, getNotes, editNote } = context
 
     useEffect(() => {
         getNotes();
@@ -18,14 +18,16 @@ const Notes = () => {
 
     const updateNote = (currentNote) => {
         ref.current.click();
-        setNote({etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag});
+        setNote({id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag});
     }
 
     const ref = useRef(null)
+    const refClose = useRef(null)
 
     const handleClick = (e) => {
         console.log("Updating the note...", note)
-        e.preventDefault();
+        editNote(note.id, note.etitle, note.edescription, note.etag)
+        refClose.current.click();
     }
 
     const onChange = (e) => {
@@ -64,7 +66,7 @@ const Notes = () => {
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button ref={refClose} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
                             <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
                         </div>
                     </div>
