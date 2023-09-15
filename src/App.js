876e52d3a044bf480014f5818f872f978218ae7f
Deep <deepvadhadiya@gmail.com>
2023-09-15 11:22:41 +0530

    add login component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
 import Alert from './components/Alert';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
           <Switch>
             <Route exact path="/" component={() => <Home />} />
             <Route exact path="/about" component={() => <About />} />
+            <Route exact path="/login" component={() => <Login />} />
+            <Route exact path="/signup" component={() => <Signup />} />
           </Switch>
         </div>
       </Router>
